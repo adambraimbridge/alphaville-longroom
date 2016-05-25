7ace96a9482e1556454376436f72843964c8d91c
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-05-25 21:29:03 +0300

    Fix app error handlers

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res) {
+	app.use(function(err, req, res, next) {
 		res.status(err.status || 500);
 		res.render('error', {
-			message: err.message,
+			message: err.errMsg || err.message,
 			error: err
 		});
 	});
@@ -41,10 +41,10 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
-		message: err.message,
+		message: err.errMsg || err.message,
 		error: {}
 	});
 });
